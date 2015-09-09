178adb1c6945d877036588d3f03d3e81804b3144
yfenghuang <yufeng.huang@qq.com>
2015-09-09 23:58:18 +0800

    Update MainWindow.xaml.cs

--- Demo/Demo/MainWindow.xaml.cs
+++ Demo/Demo/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace Demo
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("xxx, modified in GITHUB site. This is a local");
+            MessageBox.Show("xxx, modified in GITHUB site. This is a local\\\");
         }
     }
 }
